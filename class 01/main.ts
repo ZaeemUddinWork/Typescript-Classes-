0c87fd081c8ba932c7dfdfc05757ec7d884a6ab4
ZaeemUddinWork <zaeemuddinbus@gmail.com>
2024-10-09 21:38:27 +0500

    Typescript Classes

---
+++ class 01/main.ts
@@ -0,0 +1,52 @@
+/*
+    we user variable to store data like : number string and boolen
+
+
+there 3 basic data type in typescript or in coding :
+    Number = 1234
+    String = Alphabets
+    Boolean = True or False
+*/ 
+
+
+/* 
+    we can assign a varibale in 3 ways 
+
+    let 
+    const
+   ----- var -----
+    Typescript case sensitive he is ka matlab he khe 
+    if you creat a variable in capital letter and you creat another variable with small letter so in typescript they both are different
+*/
+
+
+// let Age  = 3 //it is not equal to age
+
+// let age = 6 //it is not equal to Age
+
+
+let Name = "Faaiz Bangali";
+
+
+/* 
+    if we want to print somting to check our code in typescript we will use Console.log() command in here like this : 
+*/
+
+console.log(Name);
+
+
+/* To open Cmd tab ( terminal in Vs code short key is Ctrl + ~ or `) */
+
+
+/* we neet to compile code in typescript to print it mean that your code in convert in JS code (Javascript Code) */
+
+
+/* we can compile code by typing tsc filename.ts in terminal  */
+
+/* After compile we can print code by typing Node filename.js in terminal */
+
+
+
+let num = 27 
+
+console.log(num)
